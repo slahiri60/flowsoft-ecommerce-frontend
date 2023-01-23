e1b92b2cc3bfb00309d10a94996902ad2ef1c292
Soumitra Lahiri <soumitra.lahiri@gmail.com>
2023-01-22 21:38:20 -0800

    Implemented User Registration Front End

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 import Menu from './components/nav/Menu';
 import Home from './pages/Home';
 import Login from './pages/Login';
@@ -8,6 +9,7 @@ export default function App() {
   return (
     <BrowserRouter>
       <Menu />
+      <Toaster />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
