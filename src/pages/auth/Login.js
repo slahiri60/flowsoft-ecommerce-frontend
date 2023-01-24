154bf45baf49afed2af2d203224a2126eb3b6693
Soumitra Lahiri <soumitra.lahiri@gmail.com>
2023-01-24 15:20:22 -0800

    Implemented user redirection

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -3,6 +3,7 @@ import Jumbotron from '../../components/cards/Jumbotron';
 import axios from 'axios';
 import toast from 'react-hot-toast';
 import { useAuth } from '../../context/auth';
+import { useNavigate } from 'react-router-dom';
 
 export default function Login() {
   // state
@@ -10,6 +11,7 @@ export default function Login() {
   const [password, setPassword] = useState('');
   // hook
   const [auth, setAuth] = useAuth();
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -25,6 +27,7 @@ export default function Login() {
         localStorage.setItem('auth', JSON.stringify(data));
         setAuth({ ...auth, token: data.token, user: data.user });
         toast.success('Login successful');
+        navigate('/');
       }
     } catch (err) {
       console.log(err);
