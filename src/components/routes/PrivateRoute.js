4a3101ef8d55751ba948239320fe45ed93b66c55
Soumitra Lahiri <soumitra.lahiri@gmail.com>
2023-01-28 20:49:52 -0800

    Implemented route protection for server response

--- src/components/routes/PrivateRoute.js
+++ src/components/routes/PrivateRoute.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import { useAuth } from '../../context/auth';
 import Loading from './Loading';
+import axios from 'axios';
 
 export default function PrivateRoute() {
   // context
@@ -10,11 +11,23 @@ export default function PrivateRoute() {
   const [ok, setOk] = useState(false);
 
   useEffect(() => {
-    if (auth?.token) {
-      setOk(true);
-    } else {
-      setOk(false);
-    }
+    const authCheck = async () => {
+      const { data } = await axios.get(
+        `${process.env.REACT_APP_API}/auth-check`,
+        {
+          headers: {
+            Authorization: auth?.token,
+          },
+        }
+      );
+      if (data.ok) {
+        setOk(true);
+      } else {
+        setOk(false);
+      }
+    };
+
+    if (auth?.token) authCheck();
   }, [auth?.token]);
 
   return ok ? <Outlet /> : <Loading />;
